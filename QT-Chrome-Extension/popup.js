f65693019d7aed4f53320f64fc9d32e36cd5ebde
KoreanJungYJ <jung22337@naver.com>
2018-06-10 16:21:05 +0900

    Css 파일 분리 및 텍스트 애니메이션 추가

--- QT-Chrome-Extension/popup.js
+++ QT-Chrome-Extension/popup.js
@@ -1,10 +1,11 @@
 (function init () {
   getSelectedText();
   translateEvent();
+  copyResult();
 }());
 
 
-// Get Selected Data
+// 크롬 스토리지에 저장
 function getSelectedText () {
   const inputText = document.getElementById('get-selected-text-form');
 
@@ -20,6 +21,15 @@ function getSelectedText () {
   });
 }
 
+// 번역할 텍스트 언어 설정
+function selectLanguage () {
+  const e = document.getElementById('language-list');
+  const langVal = e.options[e.selectedIndex].value;
+
+  return langVal;
+}
+
+// 클릭시 결과 가져옴
 function translateEvent () {
   const translateBtn = document.querySelector('button');
 
@@ -28,6 +38,7 @@ function translateEvent () {
   });
 }
 
+// API 정보 요청 후 실제 적용
 function getPapagoResult () {
   let papagoResult = document.getElementById('papago-translated-form');
 
@@ -36,7 +47,7 @@ function getPapagoResult () {
     const query = val.textData.join('');
     axios.post('http://localhost:3000/api/translation', {
       text: query,
-      language: 'en' // 임시 테스트용
+      language: selectLanguage()
     })
     .then(response => {
       console.log(response);
@@ -46,4 +57,25 @@ function getPapagoResult () {
       console.log(err);
     });
   });
+}
+
+// 클릭 시 번역 결과 복사
+function copyResult () {
+  const copyBtn = document.querySelector('.copy-result');
+  copyBtn.addEventListener('click', () => {
+    copyText();
+  });
+}
+
+function copyText () {
+  const papagoResult = document.getElementById('papago-translated-form');
+  const success = document.querySelector('.success-copy-text');
+
+  if (papagoResult.value !== '') {
+    papagoResult.select();
+    document.execCommand('copy');
+    // 텍스트 애니메이션 추가
+    success.classList.remove('success-animation');
+    setTimeout(() => success.classList.add('success-animation'), 0);
+  }
 }
